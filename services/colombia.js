d09a3a2e0b0e365da49ee34742265f42cfe6658f
David R <deveeup@MacBook-Pro.local>
2020-04-25 21:35:49 -0500

    validate after date in array

--- services/colombia.js
+++ services/colombia.js
@@ -1,13 +1,17 @@
-const MongoLib = require('../lib/mongo');
+const MongoLib = require("../lib/mongo");
+const moment = require("moment");
 
 class ColombiaService {
   constructor() {
-    this.collection = 'colombia';
+    this.collection = "colombia";
     this.mongoDB = new MongoLib();
   }
   async getData({ date }) {
     const data = await this.mongoDB.get(this.collection, date);
-    return data || {};
+    const length = data.length;
+    const returnDate = data[length - 1];
+
+    return returnDate || {};
   }
   async createData(data) {
     const createData = await this.mongoDB.create(this.collection, data);
